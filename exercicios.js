7128ea885c589026c602c8246d88a99e172d5bec
aeon <yasunofabio@outlook.com.br>
2022-10-29 12:48:04 -0400

    adiciona exercicios base

---
+++ exercicios.js
@@ -0,0 +1,38 @@
+function questao1() {
+    const distancia = 160; //km
+    const velocidadeMedia = 40; //km/h
+    const pesoPergaminho = 1.8; //T
+
+
+
+
+}
+
+function questao2() {
+    console.log(
+       'Uma das desvantagens será a dificuldade em manter' +
+       'todos os aparelhos seguros e protegidos de virus e invasões.' +
+       'Como tudo vai estar conectado a internet. Tudo pode ser um alvo em potencial.'
+    )
+}
+
+function questao3() {
+    console.log(
+        'Por não ser necessario conectar o aparelho a um cabo para que a maquina tenha acesso a rede.' + 
+        'Isso torna o uso da tecnologia mais pratica e reduz os custos de manutenção em cabos.'
+    )
+}
+
+function questao4() {
+    console.log(
+        'Vantagem: Custo reduzido em segurança; Custo reduzido em hardware' +
+        'Desvantagem: Informação do Usuario estara na mão de terceiros; Redução na taxa de transferencia de dados'
+    )
+}
+
+function questao5() {
+    console.log(
+        '' +
+        ''
+    )
+}
